71987b7b4c32fe5688bc620a6bfc4504de2ea9aa
Keith Bennett <kbennett@hurley-consulting.io>
2018-05-22 16:23:56 -0400

    end of day 22 angular 4

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -7,6 +7,12 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  
+  }
   heroes = HEROES;
   hero: Hero = {
     id: 1,
